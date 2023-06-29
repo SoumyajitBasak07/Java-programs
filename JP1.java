8c3dbf128ec1fbee86ea7a94e7ae026b5d3514c6
Soumyajit Basak <123021913+SoumyajitBasak07@users.noreply.github.com>
2023-06-30 02:02:41 +0530

    Add files via upload

---
+++ JP1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class JP1 {
+    public static void main (String args[]){
+
+        int n1 = 24;
+        int n2 = 25;
+        if (n1>=n2){
+            System.out.println("n1");
+        }
+        else{
+            System.out.println("n2.");
+        }
+    }
+}
